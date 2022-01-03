aff65396e0bc0164a49253c265fbf2492f5a500b
giorgiapavic <s1099316@studenti.univpm.it>
2022-01-03 10:55:53 +0100

    Aggiunta struttura base Spring

---
+++ src/main/java/it/univpm/progettoPavicSasu/ProgettoPavicSasuApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.progettoPavicSasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPavicSasuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPavicSasuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/univpm/progettoPavicSasu/ProgettoPavicSasuApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.progettoPavicSasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPavicSasuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
