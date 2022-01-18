22a4ebd574a66bfe0de2b133518b436bdf5ad0db
riccardosasu <s1100041@studenti.univpm.it>
2022-01-18 17:51:16 +0100

    aggiungo commento Measurement

--- src/main/java/it/univpm/progettoPavicSasu/model/Measurement.java
+++ src/main/java/it/univpm/progettoPavicSasu/model/Measurement.java
@@ -1,7 +1,12 @@
 package it.univpm.progettoPavicSasu.model;
 
 import java.util.Date;
-
+ /**
+  * Classe che definisce la stuttura delle misurazioni
+  * @author Giorgia Pavic
+  * @author Riccardo Sasu
+  *
+  */
 public class Measurement {
 
     protected Date date;
@@ -12,16 +17,23 @@ public class Measurement {
     public double cloud;
 
     /**
-     * @param dt the date to set
+     * Metodo che imposta la data
+     * @param dt
      */
     public void setDate(long dt) {
         this.date = new Date(dt * 1000);
     }
-
+    /**
+     * Metodo che imposta la data
+     * @param d
+     */
     public void setDate(Date d) {
         this.date = d;
     }
-
+    /**
+     * Metodo che restituisce la data
+     * @return data
+     */
     public Date getDate(){
         return this.date;
     }
