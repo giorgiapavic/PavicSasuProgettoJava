3b5452920429e45b5cddab1eaec54f9f342624cf
riccardosasu <s1100041@studenti.univpm.it>
2022-01-18 18:00:34 +0100

    aggiungo commenti ConfigurationForecast

--- src/main/java/it/univpm/progettoPavicSasu/configuration/ConfigurationForecast.java
+++ src/main/java/it/univpm/progettoPavicSasu/configuration/ConfigurationForecast.java
@@ -1,7 +1,7 @@
-
 package it.univpm.progettoPavicSasu.configuration;
 
 /**
+ * Classe che definisce gli indirizzi e le metodologie di richiesta nel API
  * @author Riccardo Sasu
  * @author Giorgia Pavic
  */
@@ -10,6 +10,10 @@ public class ConfigurationForecast {
     public final static String unit = "metric";
     public final static String token = "b199b911358fad3dbf6f4522253a09e9";
 
+    /**
+	 * Metodo che restituisce l'URL
+	 * @return URL
+	 */
     public static String getUri() {
         return url + "?units=" + unit + "&appid=" + token;
     }
