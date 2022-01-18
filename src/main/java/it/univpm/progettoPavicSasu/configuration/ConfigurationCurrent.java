e99242b050be6e9714ccae2feb57aa593a69f057
riccardosasu <s1100041@studenti.univpm.it>
2022-01-18 17:58:11 +0100

    aggiungo commenti ConfigurationCurrent

--- src/main/java/it/univpm/progettoPavicSasu/configuration/ConfigurationCurrent.java
+++ src/main/java/it/univpm/progettoPavicSasu/configuration/ConfigurationCurrent.java
@@ -1,7 +1,7 @@
-
 package it.univpm.progettoPavicSasu.configuration;
 
 /**
+ * Classe che definisce gli indirizzi e le metodologie di richiesta nel API
  * @author Riccardo Sasu
  * @author Giorgia Pavic
  */
@@ -9,7 +9,11 @@ public class ConfigurationCurrent {
 	public final static String url = "http://api.openweathermap.org/data/2.5/weather";
 	public final static String unit = "metric";
 	public final static String token = "b199b911358fad3dbf6f4522253a09e9";
-	
+ 
+	/**
+	 * Metodo che restituisce l'URL
+	 * @return URL
+	 */
 	public static String getUri() {
 		return url + "?units=" + unit + "&appid=" + token;
 	}
