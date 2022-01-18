6b7bc02a862b2ef63e75c54fb514367c0d2761bf
riccardosasu <s1100041@studenti.univpm.it>
2022-01-18 18:26:37 +0100

    aggiungo commenti Forecast

--- src/main/java/it/univpm/progettoPavicSasu/model/Forecast.java
+++ src/main/java/it/univpm/progettoPavicSasu/model/Forecast.java
@@ -4,15 +4,25 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-
+ /**
+  * Classe con l'array del meteo delle città
+  * @author Giorgia Pavic
+  * @author Riccardo Sasu
+  */
 public class Forecast {
 
     private ArrayList<CityForecast> cityArray = new ArrayList<CityForecast>();
-
+   /**
+    * Matodo per aggiungere ...
+    * @param a
+    */
     public void add(CityForecast a){
         this.cityArray.add(a);
     }
-
+    /**
+     * Metodo che restituisce un json con ...
+     * @return
+     */
     public JSONObject exportJSON(){
         JSONObject finalJson = new JSONObject();
         JSONArray res = new JSONArray();
@@ -22,7 +32,10 @@ public class Forecast {
         finalJson.put("cities",res.toList());
         return finalJson;
     }
-
+    /**
+     * Metodo che restituisce la lunghezza dell'array cityArray
+     * 
+     */
     public int size(){
         return this.cityArray.size();
     }
