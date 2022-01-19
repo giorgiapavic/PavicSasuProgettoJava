9229335c45fdf2b9d28a64700c173c1606805a9e
giorgiapavic <s1099316@studenti.univpm.it>
2022-01-19 09:33:49 +0100

    Commenti

--- src/main/java/it/univpm/progettoPavicSasu/model/Forecast.java
+++ src/main/java/it/univpm/progettoPavicSasu/model/Forecast.java
@@ -13,15 +13,14 @@ public class Forecast {
 
     private ArrayList<CityForecast> cityArray = new ArrayList<CityForecast>();
    /**
-    * Matodo per aggiungere ...
+    * Matodo per aggiungere un oggetto all'interno dell'array delle previsioni
     * @param a
     */
     public void add(CityForecast a){
         this.cityArray.add(a);
     }
     /**
-     * Metodo che restituisce un json con ...
-     * @return
+     * Metodo che restituisce un json dagli oggetti CityForecast
      */
     public JSONObject exportJSON(){
         JSONObject finalJson = new JSONObject();
@@ -39,7 +38,11 @@ public class Forecast {
     public int size(){
         return this.cityArray.size();
     }
-
+    /**
+     * Metodo restituisce l'oggetto forecast
+     * @param n
+     * @return
+     */
     public CityForecast get(int n){
         return this.cityArray.get(n);
     }
