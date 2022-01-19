8805fd71cacc754b8d10f1d51ab412eb858b3d73
giorgiapavic <s1099316@studenti.univpm.it>
2022-01-19 11:51:15 +0100

    aggiungo javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.univpm.progettoPavicSasu"},{"l":"it.univpm.progettoPavicSasu.configuration"},{"l":"it.univpm.progettoPavicSasu.Controller"},{"l":"it.univpm.progettoPavicSasu.filters"},{"l":"it.univpm.progettoPavicSasu.model"},{"l":"it.univpm.progettoPavicSasu.service"},{"l":"statistiche"}];updateSearchResults();
